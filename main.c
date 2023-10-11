2f35d6875abdd56bf5f576a9977c76821e96997f
Arnau Vives <92750813+AVC99@users.noreply.github.com>
2023-10-11 08:47:11 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include <time.h>
+
+
+int main (int argc, char *argv[]){
+
+  printf("Hello World\n");
+
+  return 0;
+}
